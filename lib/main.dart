0337e145ec6d75d1c48b318c2e8aa9b64d7e1b30
f0cuSsss <f0cuSmcsza@gmail.com>
2021-06-11 16:09:24 +0300

    Some settings [need test]

--- lib/main.dart
+++ lib/main.dart
@@ -2,30 +2,47 @@ import 'package:Applitv/services/NotificationService.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:Applitv/screens/HomeScreen.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:provider/provider.dart';
 import 'package:workmanager/workmanager.dart';
 
 void callbackDispatcher() {
   Workmanager.executeTask((taskName, inputData) {
-    NotificationService().instantNotification();
+    // NotificationService().instantNotification();
     print("bg notify");
     return Future.value(true);
   });
 }
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Workmanager.initialize(
-    callbackDispatcher,
-    isInDebugMode: true,
+  final AndroidNotificationChannel channel = AndroidNotificationChannel(
+    'high_importance_channel', // id
+    'High Importance Notifications', // title
+    'This channel is used for important notifications.', // description
+    importance: Importance.max,
   );
-  await Workmanager.registerPeriodicTask(
-    "test_workertask",
-    "test_workertask",
-    frequency: Duration(seconds: 10), // minimum is 15 min
-    inputData: {"data1": "value1", "data2": "value2"},
-    initialDelay: Duration(seconds: 5),
-  );
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
+  // flutterLocalNotificationsPlugin.resolvePlatformSpecificImplementation().
+
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Workmanager.initialize(
+  //   callbackDispatcher,
+  //   isInDebugMode: true,
+  // );
+  // await Workmanager.registerPeriodicTask(
+  //   "test_workertask",
+  //   "test_workertask",
+  //   frequency: Duration(seconds: 10), // minimum is 15 min
+  //   inputData: {"data1": "value1", "data2": "value2"},
+  //   initialDelay: Duration(seconds: 5),
+  // );
 
   // await Workmanager.registerOneOffTask();
 
@@ -34,14 +51,9 @@ void main() async {
         shortcuts: <LogicalKeySet, Intent>{
           LogicalKeySet(LogicalKeyboardKey.select): ActivateIntent(),
         },
-        child: MultiProvider(
-          child: MaterialApp(
-            debugShowCheckedModeBanner: false,
-            home: HomeScreen(),
-          ),
-          providers: [
-            ChangeNotifierProvider(create: (_) => NotificationService())
-          ],
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: HomeScreen(),
         )),
   );
 }
