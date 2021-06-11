a586d54b0c06f86da644c1cceda100c4b29c6aba
f0cuSsss <f0cuSmcsza@gmail.com>
2021-06-11 09:47:49 +0300

    BG task with delay 15 min

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,32 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:Applitv/screens/HomeScreen.dart';
 import 'package:provider/provider.dart';
+import 'package:workmanager/workmanager.dart';
 
-void main() {
+void callbackDispatcher() {
+  Workmanager.executeTask((taskName, inputData) {
+    NotificationService().instantNotification();
+    print("bg notify");
+    return Future.value(true);
+  });
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Workmanager.initialize(
+    callbackDispatcher,
+    isInDebugMode: true,
+  );
+  await Workmanager.registerPeriodicTask(
+    "test_workertask",
+    "test_workertask",
+    frequency: Duration(seconds: 10), // minimum is 15 min
+    inputData: {"data1": "value1", "data2": "value2"},
+    initialDelay: Duration(seconds: 5),
+  );
+
+  // await Workmanager.registerOneOffTask();
+
   runApp(
     Shortcuts(
         shortcuts: <LogicalKeySet, Intent>{
